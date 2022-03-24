482a5667702c92f1ccf8e3b3d3a784dfdfbfcb7e
withers56 <william.k.withers@gmail.com>
2022-03-24 15:58:22 -0500

    completed albums_migration exercises

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    release_date INT(4) NOT NULL,
+    sales FLOAT NOT NULL,
+    genre VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id)
+);
