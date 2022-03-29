337eb4f5a291f122c3eb16a88566a29a8f5eb7ee
withers56 <william.k.withers@gmail.com>
2022-03-29 11:42:44 -0500

    completed where exercise

---
+++ where_exercises.sql
@@ -0,0 +1,38 @@
+-- Create a file named where_exercises.sql. Make sure to use the employees database before you begin.
+-- Find all employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+-- Find all employees whose last name starts with 'E' — 7,330 rows.
+SELECT *
+FROM employees
+WHERE last_name like 'e%';
+-- Find all employees with a 'q' in their last name — 1,873 rows.
+SELECT *
+FROM employees
+WHERE last_name like '%q%';
+
+# Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows.
+SELECT *
+FROM employees
+WHERE first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya';
+# Add a condition to the previous query to find everybody with those names who is also male — 441 rows.
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+and gender = 'M';
+# Find all employees whose last name starts or ends with 'E' — 30,723 rows.
+SELECT *
+FROM employees
+WHERE last_name like 'e%'
+or last_name like '%e';
+# Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
+SELECT *
+FROM employees
+WHERE last_name like 'e%'
+   and last_name like '%e';
+# Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
+SELECT *
+FROM employees
+WHERE last_name like '%q%'
+and last_name not like '%qu%';
