e6d871a9f332101eae0a29af8a649eea99ca0f5a
withers56 <william.k.withers@gmail.com>
2022-03-29 15:28:39 -0500

    completeed lmit-exercise

---
+++ limit_exercises.sql
@@ -0,0 +1,14 @@
+SELECT DISTINCT last_name
+FROM employees
+ORDER BY last_name DESC
+LIMIT 10;
+
+SELECT emp_no, salary
+FROM salaries
+ORDER BY salary DESC
+LIMIT 5;
+
+SELECT emp_no, salary
+FROM salaries
+ORDER BY salary DESC
+LIMIT 5 OFFSET 45;
